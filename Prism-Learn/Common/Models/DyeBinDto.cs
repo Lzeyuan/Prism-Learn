8acab7b7c9fdeaa22567a300803532d595b7b96f
LEZA <1046527915@qq.com>
2023-04-28 17:06:40 +0800

    尝试MVVM

---
+++ Prism-Learn/Common/Models/DyeBinDto.cs
@@ -0,0 +1,95 @@
+﻿using Prism.Mvvm;
+using System.Windows;
+
+namespace Prism_Learn.Common.Models {
+    public class DyeBinDto : BindableBase {
+
+        public enum State {
+            kNone,
+            kCoverGlass,
+            kPressed,
+            kSubside,
+            kLoadSample,
+            kHematoxylin,
+            kEAOG,
+            kBuffer,
+            kAbsoluteAlcohol
+        }
+
+        private string image = "";
+        public string Image {
+            get { return image; }
+            set {
+                image = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private int number;
+        public int Number {
+            get { return number; }
+            set {
+                number = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string backgroundColor = "#FFFFFF";
+        public string BackgroundColor {
+            get { return backgroundColor; }
+            set {
+                backgroundColor = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private Visibility show = Visibility.Collapsed;
+        public Visibility Show {
+            get { return show; }
+            set {
+                show = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public void SetState(State state) {
+
+
+            if (state == State.kNone) {
+                BackgroundColor = "#F6F6F6";
+                Show = Visibility.Collapsed;
+            } else if (state == State.kCoverGlass) {
+                BackgroundColor = "#FFFFFF";
+                Show = Visibility.Collapsed;
+            } else {
+                BackgroundColor = "#FFFFFF";
+                Show = Visibility.Visible;
+            }
+
+
+            switch (state) {
+                case State.kPressed:
+                    Image = "/Image/ic_dyebin_pressed.png";
+                    break;
+                case State.kSubside:
+                    Image = "/Image/ic_dyebin_subside.png";
+                    break;
+                case State.kLoadSample:
+                    Image = "/Image/ic_dyebin_load_sample.png";
+                    break;
+                case State.kHematoxylin:
+                    Image = "/Image/ic_dyebin_hematoxylin.png";
+                    break;
+                case State.kEAOG:
+                    Image = "/Image/ic_dyebin_eaog.png";
+                    break;
+                case State.kBuffer:
+                    Image = "/Image/ic_dyebin_buffer.png";
+                    break;
+                case State.kAbsoluteAlcohol:
+                    Image = "/Image/ic_dyebin_absolute_alcohol.png";
+                    break;
+            }
+        }
+    }
+}
--- Prism-Learn/ViewModels/SettingViewModel.cs
+++ Prism-Learn/ViewModels/SettingViewModel.cs
@@ -1,4 +1,69 @@
-﻿namespace Prism_Learn.ViewModels {
-    class SettingViewModel {
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using Prism_Learn.Common.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Prism_Learn.ViewModels {
+    public class SettingViewModel : BindableBase {
+
+        int addCount = 0;
+
+        public SettingViewModel() {
+            dyeBinDtos = new ObservableCollection<DyeBinDto>();
+
+            for (int i = 0; i < 5; ++i) {
+                for (int j = 0; j < 6; ++j) {
+                    DyeBinDto dyeBinDto = new();
+                    dyeBinDto.Number = j * 5 + i;
+                    dyeBinDto.SetState(DyeBinDto.State.kNone);
+                    dyeBinDtos.Add(dyeBinDto);
+                }
+            }
+
+            DyeBinDto.State state = DyeBinDto.State.kNone;
+            TestClick = new DelegateCommand(() => {
+                SetDyeBinDtosState(addCount, state);
+                ++addCount;
+                if (addCount == 2) {
+                    ++state;
+                    if (state > DyeBinDto.State.kAbsoluteAlcohol) state = DyeBinDto.State.kNone;
+                    addCount = 0;
+                }
+            });
+        }
+
+        void SetDyeBinDtosState(int index, DyeBinDto.State state) {
+            if (0 > index || index > 29) {
+                throw new ArgumentOutOfRangeException("DyeBinDtos:" + nameof(index));
+            }
+            int realIndex = 6 * (index % 5) + index / 5;
+            DyeBinDtos[realIndex].SetState(state);
+        }
+        public DelegateCommand TestClick { get; private set; }
+        private ObservableCollection<DyeBinDto> dyeBinDtos;
+
+        public ObservableCollection<DyeBinDto> DyeBinDtos {
+            get { return dyeBinDtos; }
+            set { dyeBinDtos = value; RaisePropertyChanged(); }
+        }
     }
+
+    public class AddOneConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            // 将绑定的数据转换为int类型并加1
+            int intValue = (int)value;
+            return intValue + 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+
 }
